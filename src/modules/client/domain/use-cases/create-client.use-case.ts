230440b20143d1c2e477dee2467931edaed7541f
viniciusdlx <viniciuslx204@hotmail.com>
2023-05-07 02:40:01 -0300

    WIP(client): Client Module

---
+++ src/modules/client/domain/use-cases/create-client.use-case.ts
@@ -0,0 +1,29 @@
+import * as admin from 'firebase-admin';
+
+export class CreateClientUseCase {
+    async execute() {
+        admin.initializeApp({
+            credential: admin.credential.cert({
+                projectId: 'YOUR_PROJECT_ID',
+                clientEmail: 'YOUR_CLIENT_EMAIL',
+                privateKey: 'YOUR_PRIVATE_KEY',
+            }),
+            databaseURL: 'https://YOUR_PROJECT_ID.firebaseio.com',
+        });
+
+        const uid = 'some-uid'; // ID do usuário do seu sistema
+        const additionalClaims = {
+            cliente: 'CLIENTE1',
+            tokens: {
+                App_TOKEN: 'abcdc123',
+                App_Key: 'adwada232',
+                WebexToken: '239dw9awd',
+                IuguToken: 'dawdaw9daw',
+            },
+        };
+
+        const customToken = await admin
+            .auth()
+            .createCustomToken(uid, additionalClaims);
+    }
+}
